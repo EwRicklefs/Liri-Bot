0d1bc64ebfcb122113ae54bd9d1a19572fd2b42c
EwRicklefs <ew.ricklefs@gmail.com>
2019-03-26 14:54:26 -0700

    building out framework, mostly just needs apis

--- liri.js
+++ liri.js
@@ -10,43 +10,74 @@ var value = process.argv[3]
 
 //*****Functions******
 function concert() {
+    var apiCall = 'https://rest.bandsintown.com/artists/' + value + '/events?app_id=codingbootcamp'
+    var apiValue
+    var venue
+    var location
+    var eventDate
     //Spotify API call based off value
-    //console log 
-/*      name of venue
-        venue location
-        date of event (mm/dd/yyyy, use moment)
-*/
+    console.log('Venue is ' + venue)
+    console.log('Venue location is' + location)
+    console.log('Date of the event is ' + moment(eventDate, "MM/DD/YYYY"))
+    
 }
 
 function spotify() {
-    //spotify API call based off value
-    //console log
-/*
-        artist(s)
-        song name
-        preview link from spotify
-        album
-*/
-//with no song default to 'the sign' by ace of bass
+    var apiCall
+    var apiResponse
+    var artist
+    var songName
+    var prevLink
+    var album
+    //how to handle no response value from spotify?
+
+    if (value) {
+        //values are set to spotify api call values
+    } else {
+        artist = 'Ace of Base'
+        songName = 'The Sign'
+        prevLink //put link here
+        album //put album here
+    }
+
+    console.log('Artist is ' + artist)
+    console.log('Song name is ' + songName)
+    console.log('Preview link - ' + prevLink)
+    console.log ('Albume is ' + album)
 }
 
 function movie() {
-    //OMDB call based off value (axios package)
-/*
-log..  * Title of the movie.
-       * Year the movie came out.
-       * IMDB Rating of the movie.
-       * Rotten Tomatoes Rating of the movie.
-       * Country where the movie was produced.
-       * Language of the movie.
-       * Plot of the movie.
-       * Actors in the movie.
-*/
+    var apiCall
+    var apiResponse
 
-//with no value default to "mr. nobody"
+    //movie values
+    var title
+    var year
+    var imdbRat
+    var rtRat
+    var prodCountry
+    var language
+    var plot
+    var actors
+
+    if (value) {
+        //set values to API call values
+    } else {
+        title = 'Mr. Nobody'
+        //set rest of the values here
+    }
+
+    console.log('Title is ' + title)
+    console.log('Year the movie came out is ' + year)
+    console.log('IMDB Rating is ' + imdbRat + '. Rotten tomatoes rating is ' + rtRat + '.')
+    console.log('Country of production is ' + prodCountry)
+    console.log('The language of the movie is ' + language)
+    console.log('Plot summary: ' + plot)
+    console.log('Actors in the movie are ' + actors)
 }
 
 function doWhat() {
+    
 /*
 Uses the text in random.txt to make a call
 should take in a function call (spotify/movie/concerts)
@@ -55,11 +86,9 @@ should take in a function call (spotify/movie/concerts)
 }
 
 
-//executes proper function based off user input
+//*****Body of the code*****/
 if (command ==='concert-this') {
     concert()
-
- 
 } else if (command==='spotify-this-song') {
     spotify()
 } else if (command === 'movie-this') {
